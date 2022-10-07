a5740da96e91e78ab17da38a7c52c07f5a8706e7
Sara Pellnor <sara.ahlquist@hotmail.com>
2022-10-07 16:31:54 +0100

    kommentarer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Skapade konstanter i JS
 const logInBtn = document.querySelector (".logIn");
 const logOutBtn = document.querySelector (".logOut");
+const makeUserBtn = document.querySelector (".makeUser");
 const h1 = document.querySelector ("h1");
 const obs = document.querySelector (".obs");
 const paragraph = document.createElement ("p");
@@ -10,9 +11,10 @@ const username = document.querySelector (".username");
 const password = document.querySelector (".password");
 const newUserName = document.querySelector (".newUserName");
 const newPassword = document.querySelector (".newPassword");
-const makeUserBtn = document.querySelector (".makeUser");
 const confirmNewUser = document.querySelector (".confirmNewUser");
 const video = document.querySelector ("video");
+
+
 
 //Inloggningskontroll//
 function init() { //döpt funktionen till init
@@ -21,7 +23,10 @@ function init() { //döpt funktionen till init
     }
 }
 
+
 init(); //kör funktionen init
+
+
 
 
 //Min array på godkända användare
@@ -41,15 +46,14 @@ const users = [
         username: "sam",
         password: "33333"
     }
-
 ];
+
+
 
 if (!localStorage.getItem("users")){ //om nyckeln "users" inte finns,
 localStorage.setItem("users", JSON.stringify (users)); //Sparas arrayen i LS i nyckeln: "users"
 }
 
-//lyssnar på "ny användare-knappen"
-makeUserBtn.addEventListener("click", createNewUser);
 
 
 //Funktionen "skapa ny användare"
@@ -59,64 +63,65 @@ function createNewUser () {
         const usersList = JSON.parse (localStorage.getItem ("users")); //spara ner arrayen till JS i "usersList" för att lägga till användare i lisan
         usersList.push ({username: newUserName.value, password: newPassword.value}); //pusha in ny användare och lösenord i listan
         localStorage.setItem("users", JSON.stringify (usersList)); //skriv över informationen i nyckeln: "users" och skickar upp till LS igen.
-        confirmNewUser.innerHTML = "Nu har du skapat en ny användare, nu kan du logga in!";
-    } else { //annars skicka ut ett meddelande
+        confirmNewUser.innerHTML = "Nu har du skapat en ny användare, nu kan du logga in!"; //visar ett meddelande if
+    } else { //else skicka ut ett meddelande
         confirmNewUser.innerHTML = "Du måste ange nytt namn och lösenord..";
     }
 }
 
 
-//logg in-knappen startar funktionen "logInFunction"
+
+//hämtar knapparna med klickfunktion
 logInBtn.addEventListener("click", logInFunction);
+logOutBtn.addEventListener("click", leave);
+makeUserBtn.addEventListener("click", createNewUser);
+
 
 
 
 //Funktionen "logga in användare"
 function logInFunction () {
-
-    //hämtar listan med användare i
-
-    const usersList = JSON.parse (localStorage.getItem ("users"));
-
-    for (const i of usersList) {
-        if (username.value == i.username && password.value == i.password){
-            localStorage.setItem ("user", JSON.stringify (username.value));
-            success ();
-            return; 
-        }
+    const usersList = JSON.parse (localStorage.getItem ("users")); //hämtar listan med användare i
+    for (const i of usersList) { //loop som går igenom usersList med med constanten "i".
+        if (username.value == i.username && password.value == i.password){ //om i hittar rätt
+            localStorage.setItem ("user", JSON.stringify (username.value)); //skickar upp user till LS
+            success (); //kör funktionen success
+            return; //avbryter loopen
+        } //visar felmeddelande i diven .obs och paragrafen ändrar färg till röd.
         paragraph.innerHTML = "Användarnamn eller lösenord finns inte. Skapa en ny användare eller försök igen";
         obs.appendChild(paragraph);
         paragraph.style.color = "red";
-
     }
 }
 
+
+
+//funktionen godkänd inloggning
 function success () {
-    
-    h1.innerHTML = "Välkommen " + JSON.parse (localStorage.getItem ("user"));
-    h1.style.color = "black";
-    logInBtn.style.display = "none";
-    logOutBtn.style.display = "block";
-    loginForm.style.display = "none";
-    paragraph.style.display = "none"; 
-    newForm.style.display = "none"; 
+    h1.innerHTML = "Välkommen " + JSON.parse (localStorage.getItem ("user")); //hämtar användarnamnet från LS och lägger in det i välkomstexten. 
+    h1.style.color = "none"; //ändrar tillbaka färgen till standard
+    logInBtn.style.display = "none"; //gömmer logginknappen
+    logOutBtn.style.display = "block";//visar loggutknappen
+    loginForm.style.display = "none";//gömmer logginformuläret
+    paragraph.style.display = "none"; //gömmer paragrafen
+    newForm.style.display = "none"; //gömmer nya-nvändare-formuläret
     video.style.display = "block";//cool bild från internet 
 }
 
-logOutBtn.addEventListener("click", leave);
 
 
-function leave () {
 
-    h1.innerHTML = "Hej då " + JSON.parse (localStorage.getItem ("user"));
-    h1.style.color = "pink";
-    h1.style.fontSize = "500%";
-    logInBtn.style.display = "block";
-    logOutBtn.style.display = "none";
-    loginForm.style.display = "block";
-    paragraph.style.display = "none";
-    newForm.style.display = "block";
-    localStorage.removeItem ("user");
-    video.style.display = "none";
-    confirmNewUser.innerHTML = "";
+//loggautfunktionen
+function leave () { 
+    h1.innerHTML = "Hej då " + JSON.parse (localStorage.getItem ("user")); //ändrar h1 till hej då "användare"
+    h1.style.color = "pink"; //gör om texten till rosa
+    h1.style.fontSize = "500%"; //gör bokstäverna större
+    logInBtn.style.display = "block"; //visar logginknappen igen
+    logOutBtn.style.display = "none"; //döljer loggutknappen
+    loginForm.style.display = "block"; //logginformuläret visas
+    paragraph.style.display = "none"; //paragrafen försvinner
+    newForm.style.display = "block"; //skapa ny användar-formuläret visas
+    localStorage.removeItem ("user"); //tar bort användaren från LS
+    video.style.display = "none"; //videon visas inte
+    confirmNewUser.innerHTML = "none"; //tar bort "bekräftelsetexten" på ny användare
 }
